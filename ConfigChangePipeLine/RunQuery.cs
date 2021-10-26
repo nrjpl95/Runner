dde924f556b45cf7532f84d1e030994f478e9c56
nrjpl95 <48802710+nrjpl95@users.noreply.github.com>
2021-10-27 00:38:19 +0530

    Add files via upload

---
+++ ConfigChangePipeLine/RunQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace ConfigChangePipeLine
+{
+    public class RunQuery
+    {
+        public void Run()
+        {
+            string sqlConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\neeraj.kumar\source\repos\ConfigChangePipeLine\ConfigChangePipeLine\TestDb.mdf;Integrated Security=True";
+
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            string script = File.ReadAllText(Path.Combine(sCurrentDirectory+ @"\Queries\query.sql"));
+
+            SqlConnection conn = new SqlConnection(sqlConnectionString);
+
+            Server server = new Server(new ServerConnection(conn));
+
+            server.ConnectionContext.ExecuteNonQuery(script);
+        }
+    }
+}
